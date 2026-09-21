cbea4965ee0f4fc179f867fa74ae8f41eec665f1
Henry Wisdom <henry6769wisdom@gmail.com>
2026-09-22 00:38:16 +0100

    Add hardware_multiplier entity and architecture

--- 06. Hardware-Software Co-Design/hdw_sfw_design.vhd
+++ 06. Hardware-Software Co-Design/hdw_sfw_design.vhd
@@ -1,1 +1,54 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
+entity hardware_multiplier is
+    Port (
+        clk        : in  std_logic;
+        reset      : in  std_logic;
+
+        start      : in  std_logic;
+
+        A          : in  std_logic_vector(31 downto 0);
+        B          : in  std_logic_vector(31 downto 0);
+
+        result     : out std_logic_vector(63 downto 0);
+        done       : out std_logic
+    );
+end hardware_multiplier;
+
+architecture Behavioral of hardware_multiplier is
+
+begin
+
+    process(clk)
+    begin
+
+        if rising_edge(clk) then
+
+            if reset = '1' then
+
+                result <= (others => '0');
+                done   <= '0';
+
+            else
+
+                done <= '0';
+
+                if start = '1' then
+
+                    result <= std_logic_vector(
+                        unsigned(A) * unsigned(B)
+                    );
+
+                    done <= '1';
+
+                end if;
+
+            end if;
+
+        end if;
+
+    end process;
+
+end Behavioral;
